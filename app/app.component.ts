5fdcea1ad67ab6d39636ef0fc90ce32a40a03492
Bjørn Arve Lagim <bjornarvelagim@MacBookWIFI.lan>
2016-11-10 23:49:03 +0100

    minor fix

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello Angular</h1>`
+    templateUrl: 'app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+  // [ ] means propery binding - Component to DOM
+  // ( ) means event binding - DOM to Component
+  title = 'Customer App';
+  name = 'Ward';
+  wardsColor = 'green';
+  customers: [
+    { id: '1', name: 'Boris' },
+    { id: '2', name: 'Vivian' },
+    { id: '3', name: 'Robert' },
+    { id: '4', name: 'Paul' },
+    { id: '5', name: 'Eva' },
+    { id: '6', name: 'Tom' }
+    ];
+
+  changeSuitColor(){
+    this.wardsColor = this.wardsColor === 'blue' ? 'red' : 'blue';
+  }
+
+}
